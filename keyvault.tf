414511d4bb60bbce2ea04df54eec61111cdeae2c
bhavyar-806 <bhavyar806@gmai.com>
2025-06-06 15:41:38 +0530

    first commit

---
+++ keyvault.tf
@@ -0,0 +1,14 @@
+resource "azurerm_key_vault" "kv" {
+  name                        = "kv-dotnet-${random_string.suffix.result}"
+  location                    = var.location
+  resource_group_name         = var.resource_group_name
+  tenant_id                   = var.tenant_id
+  sku_name                    = "standard"
+  purge_protection_enabled    = true
+}
+
+resource "azurerm_key_vault_secret" "example" {
+  name         = "DbPassword"
+  value        = "SuperSecret123"
+  key_vault_id = azurerm_key_vault.kv.id
+}
---
+++ main.tf
@@ -0,0 +1,44 @@
+provider "azurerm" {
+    features {}
+    subscription_id = "fd240ce0-897d-4c0e-a243-662d89ccda9a"
+  
+}
+resource "azurerm_resource_group" "app-rg" {
+    name     =  var.resource_group_name
+    location = var.location
+  
+}
+
+
+resource "azurerm_app_service_plan" "asp" {
+  name                = "dotnet-asp"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "app" {
+  name                = "dotnetapp-${random_string.suffix.result}"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  app_service_plan_id = azurerm_app_service_plan.asp.id
+
+  site_config {
+    dotnet_framework_version = "v4.0"
+    scm_type                 = "LocalGit"
+  }
+
+  app_settings = {
+    "SOME_KEY" = "some-value"
+  }
+
+  connection_string {
+    name  = "Database"
+    type  = "SQLServer"
+    value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
+  }
+}
