b16506a376c2f9262fdf5446d65b88f5d0765dc6
bhavyar-806 <bhavyar806@gmai.com>
2025-06-07 11:03:01 +0530

    chanmges

--- keyvault.tf
+++ keyvault.tf
@@ -10,17 +10,17 @@ resource "azurerm_key_vault" "kv" {
 resource "azurerm_role_assignment" "keyvault_access" {
   scope                = azurerm_key_vault.my_kv.id
   role_definition_name = "Key Vault Secrets User"
-  principal_id         = "891ced47-c1cb-475b-8345-f1819eddf85a"  # 👈 your SP object ID
+  principal_id         = "891ced47-c1cb-475b-8345-f1819eddf85a" # 👈 your SP object ID
 }
 
- # or system identity
+# or system identity
 
 
 resource "azurerm_key_vault_secret" "example" {
   name         = "DbPassword"
   value        = "SuperSecret123"
   key_vault_id = azurerm_key_vault.kv.id
- 
+
 }
 
 resource "random_string" "suffix" {
